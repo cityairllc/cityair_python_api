d183fa083c6cd9b73dbb1f94e7e7fd2e6cfca7e0
egorkorovin <turokg@yahoo.com>
2019-03-28 19:57:14 +0700

    added corr graphs. major refactor of graphs

--- utils.py
+++ utils.py
@@ -1,7 +1,10 @@
-def filter(df_or_series, window=5, tol=0.03, shift=-1):
+import pandas as pd
+
+def filter_df(df_or_series, window=5, tol=0.03, shift=-1):
     if isinstance(df_or_series, pd.Series):
         series = df_or_series
-        return series[abs(series - series.rolling(window).mean().shift(-shift))/series < tol]
+        series = series[abs(series - series.rolling(window).mean().shift(-shift)) / series < tol]
+        return
     elif isinstance(df_or_series, pd.DataFrame):
         res = pd.DataFrame()
         for col in df_or_series:
@@ -10,3 +13,11 @@ def filter(df_or_series, window=5, tol=0.03, shift=-1):
         return res
     else:
         raise TypeError('should be pd.Series or pd.DataFrame')
+
+def dropnas(series):
+    if not isinstance(series, pd.Series):
+        raise TypeError('should be pd.Series')
+    df = pd.DataFrame(series)
+    df['index'] = series.index
+    df.dropna(inplace = True)
+    return pd.Series(data = df.iloc[:, 0], index = df['index'])
