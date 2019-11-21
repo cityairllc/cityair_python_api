22a71ce2a457fddcd796fd47a54205b4e84043b8
ek <turokg@icloud.com>
2019-11-22 00:10:59 +0700

    major refactor added private cached_properties

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='cityair-api',
-    version='0.2.30',
+    version='0.2.31',
     packages=['cityair_api'],
     url='https://github.com/cityairltd/cityair_python_api',
     license='Apache 2.0',
