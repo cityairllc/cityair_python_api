6485a726cc9711be82e2760bc88458e6d1df533b
ek <turokg@yahoo.com>
2019-11-22 17:28:16 +0700

    bugfix. dealing with devices that have no data in tail

--- cityair_api/request.py
+++ cityair_api/request.py
@@ -42,7 +42,6 @@ class CityAirRequest:
         self.timeout = kwargs.get('timeout', 100)
         self.user = user
         self.psw = psw
-        print('dsa')
 
 
     @cached_property
--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name='cityair-api',
-    version='0.2.31',
+    version='0.2.33',
     packages=['cityair_api'],
     url='https://github.com/cityairltd/cityair_python_api',
     license='Apache 2.0',
     author='Egor Korovin',
     author_email='ek@cityair.ru',
     description='A python api to access data from cityair.io',
-    classifiers=['Development Status :: 4 - Beta']      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package]
+    classifiers=['Development Status :: 5 - Production/Stable']      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package]
 )
 
 
