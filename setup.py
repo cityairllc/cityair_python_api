d88983cd841284e3b4845f00492e473a1f8586fa
ek <turokg@yahoo.com>
2019-12-03 18:40:40 +0700

    proper requirements in setup

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from distutils.core import setup
 
 setup(
     name='cityair-api',
-    version='0.2.33',
+    version='0.2.35',
     packages=['cityair_api'],
     url='https://github.com/cityairltd/cityair_python_api',
     license='Apache 2.0',
     author='Egor Korovin',
     author_email='ek@cityair.ru',
     description='A python api to access data from cityair.io',
-    classifiers=['Development Status :: 5 - Production/Stable']      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package]
+    classifiers=['Development Status :: 5 - Production/Stable'],
+    # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package]
+    install_requires=[
+        'pandas', 'requests', 'progressbar2', 'cached-property'],
 )
-
-
