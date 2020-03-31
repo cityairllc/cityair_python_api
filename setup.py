a64d0456d33e00acc7312fcdc51d1c3919b90c72
ek <ek@cityair.io>
2020-03-31 14:15:47 +0700

    updated packages and requirements.txt

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from distutils.core import setup
 
 setup(
-    name='cityair-api',
-    version='0.2.42',
-    packages=['cityair_api'],
-    url='https://github.com/cityairltd/cityair_python_api',
-    license='Apache 2.0',
-    author='Egor Korovin',
-    author_email='ek@cityair.ru',
-    description='A python api to access data from cityair.io',
-    classifiers=['Development Status :: 5 - Production/Stable'],
-    # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package]
-    install_requires=[
-        'pandas', 'requests', 'progressbar2==3.47.0', 'cached-property'],
+        name='cityair-api',
+        version='0.2.44',
+        packages=['cityair_api'],
+        url='https://github.com/cityairltd/cityair_python_api',
+        license='Apache 2.0',
+        author='Egor Korovin',
+        author_email='ek@cityair.io',
+        description='python api to access data from cityair.io',
+        classifiers=['Development Status :: 5 - Production/Stable'],
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as
+        # the current state of your package]
+        install_requires=[
+                'pandas', 'requests', 'progressbar2==3.50.1',
+                'cached-property'],
 )
-
