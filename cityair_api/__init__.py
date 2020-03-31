f18aeb5ccfd453a2b25d05ed20f819decd0f32b4
ek <ek@cityair.io>
2020-03-31 17:32:41 +0700

    added short alias for CityAirRequest

--- cityair_api/__init__.py
+++ cityair_api/__init__.py
@@ -1,3 +1,6 @@
-from .request import CityAirRequest, Period
-from .utils import prep_df, prep_dicts, USELESS_COLS, RIGHT_PARAMS_NAMES, to_date, timeit, debugit, unpack_cols
-from .exceptions import EmptyDataException, CityAirException, ServerException, NoAccessException
+from .request import CityAirRequest, Period, CAR
+from .exceptions import (
+    EmptyDataException, CityAirException,
+    ServerException,
+    NoAccessException,
+)
