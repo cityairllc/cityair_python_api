78bc70f9f7525ac1254e173d9437e258bcaf4584
ek <ek@cityair.io>
2020-07-03 15:55:38 +0700

    removed echoing of vars

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
         name='cityair-api',
-        version='0.2.66',
+        version='0.2.67',
         packages=['cityair_api'],
         url='https://github.com/cityairltd/cityair_python_api',
         license='Apache 2.0',
