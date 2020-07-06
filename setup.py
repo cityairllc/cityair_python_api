6cddf8eb201198068f8cf52820dcee5de3004f9b
ek <turokg@icloud.com>
2020-07-07 00:17:14 +0700

    made timezone anaware

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
         name='cityair-api',
-        version='0.2.68',
+        version='0.2.69',
         packages=['cityair_api'],
         url='https://github.com/cityairltd/cityair_python_api',
         license='Apache 2.0',
