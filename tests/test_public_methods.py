3cf463c8229e862db3128592e2b67785c8b0280e
ek <ek@cityair.io>
2020-08-25 20:08:19 +0700

    added tests for public methods

---
+++ tests/test_public_methods.py
@@ -0,0 +1,30 @@
+import random
+import pandas as pd
+
+from api_test_case import CityAirApiTestCase
+
+
+class TestMainMethods(CityAirApiTestCase):
+    def setup(self):
+        self.serial_number = random.choice(self.r.get_devices())
+        self.station_id = random.choice(self.r.get_stations())
+
+    def test_get_devices(self):
+        devices = self.r.get_devices()
+        assert isinstance(devices, list)
+
+    def test_get_device_data(self):
+        df = self.r.get_device_data(self.serial_number, take_count=5)
+        assert isinstance(df, pd.DataFrame)
+
+    def test_get_stations(self):
+        stations = self.r.get_stations()
+        assert isinstance(stations, list)
+
+    def test_get_station_data(self):
+        df = self.r.get_station_data(self.station_id)
+        assert isinstance(df, pd.DataFrame)
+
+    def test_get_locations(self):
+        locations = self.r.get_locations()
+        assert isinstance(locations, list)
